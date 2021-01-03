4209e8498c8b4892178e04f6d39f33db910a93f7
Angelo Geulin <angelogeulin123@gmail.com>
2021-01-03 14:51:36 +0800

    ws101: admissions: add support for HTML5 only mode in requirements page

--- admissions/requirements/index.js
+++ admissions/requirements/index.js
@@ -3,6 +3,35 @@
 alert('Hello, World!');
 */
 
+function pageInit() {
+    // Highlight the default opened tab
+    document.getElementById('tab-button-default-open').className += " active";
+    
+    // Hide all the page seperators in page body.
+    const page_seps = document.getElementsByClassName("page-body-content-tab-sep");
+    for (i = 0; i < page_seps.length; i++) {
+        page_seps[i].style.display = "none";
+    }
+    
+    // Hide all tab panes that are marked inactive
+    const default_tab_opened = "Freshmen";
+    let content_tabs = document.getElementsByClassName("page-body-content-tab");
+    for (i = 0; i < content_tabs.length; i++) {
+        if (content_tabs[i].id === default_tab_opened) {
+        content_tabs[i].className += " active"
+        }
+        if (content_tabs[i].id !== default_tab_opened) {
+        content_tabs[i].className += " inactive"
+        }
+    }
+
+    // Hide HTML5 tabpane.
+  document.getElementsByClassName('page-body-content-tabpane html5')[0].style.display = "none";
+
+  // Show JavaScript tabpane.
+  document.getElementsByClassName('page-body-content-tabpane jscript')[0].style.display = "block";
+}
+
 function openTab(event, tab_name) {
     console.log("Function" + this + "triggered.");
     let content_tabs = document.getElementsByClassName("page-body-content-tab");
